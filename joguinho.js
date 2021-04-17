f730b32db4975c53ca49782be8dd36cc718093d1
David Sodré Lins <david.silva@acpgroup.com.br>
2021-04-17 08:39:59 -0300

    Joguinho com Player

--- joguinho.js
+++ joguinho.js
@@ -3,28 +3,49 @@ faça um joguinho de advinhação que dê
 pra jogar com 2 usuarios.
 
 1- acrescente a informação de quantas
-vezes foi chutado para acertar
+vezes foi chutado para acertar OK
 2- coloque o nome dos jogadores
 3- crie um rankig
 
 */
 
 // estrutura de repetição
-var pensei = 21
-var numero = null
-var contTentativas = 0
-do{
-  numero = Number(prompt("digite um numero"));
-  if(numero<pensei){
-    console.log("--------")
-    console.log("é maior que " + numero)
+function ler() {
+  var x = Number(prompt("digite um numero"));
+  return x;
+}
+
+var jogadores = [];
+var pensei = 21;
+var numero = null;
+var contTentativas = 0;
+var player = { nome: "", pontuacao: 0 };
+
+player.nome = prompt("digite o nome do jogador");
+
+do {
+  contTentativas = contTentativas + 1;
+  numero = ler();
+
+  if (numero < pensei) {
+    console.log("--------");
+    console.log(player.nome + " é maior que " + numero);
   }
-  if(numero>pensei){
-    console.log("--------")
-    console.log("é menor que " + numero)
+  if (numero > pensei) {
+    console.log("--------");
+    console.log(player.nome + " é menor que " + numero);
   }
-  if(numero === pensei){
-    console.log("Acertô mizeravi! ")
+  if (numero === pensei) {
+    console.log(player.nome + " acertô mizeravi! ");
+    console.log("Numero de tentativas " + contTentativas);
+
+    player.pontuacao =
+      contTentativas < player.pontuacao || player.pontuacao === 0
+        ? contTentativas
+        : player.pontuacao;
+
+    jogadores.push(player);
   }
-  // contTentativas = contTentativas + 1;
-}while (numero !== pensei);
+} while (numero !== pensei);
+
+console.log(jogadores);
