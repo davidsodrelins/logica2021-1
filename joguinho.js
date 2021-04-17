5c071e400878d83dd22c0a5275273f4748b68efb
David Sodré Lins <david.silva@acpgroup.com.br>
2021-04-17 09:49:41 -0300

    Muqueca de Reinaldo

--- joguinho.js
+++ joguinho.js
@@ -7,6 +7,9 @@ vezes foi chutado para acertar OK
 2- coloque o nome dos jogadores
 3- crie um rankig
 
+
+Atualize para o permitir que 
+o usuario continue jogando ou saia.
 */
 
 // estrutura de repetição
@@ -16,36 +19,78 @@ function ler() {
 }
 
 var jogadores = [];
-var pensei = 21;
+var pensei = Math.floor(Math.random() * 100);
 var numero = null;
 var contTentativas = 0;
 var player = { nome: "", pontuacao: 0 };
 
-player.nome = prompt("digite o nome do jogador");
+menuGeral()
 
-do {
-  contTentativas = contTentativas + 1;
-  numero = ler();
+function jogar() {
+  do {
+    contTentativas = contTentativas + 1;
+    numero = ler();
 
-  if (numero < pensei) {
-    console.log("--------");
-    console.log(player.nome + " é maior que " + numero);
-  }
-  if (numero > pensei) {
-    console.log("--------");
-    console.log(player.nome + " é menor que " + numero);
-  }
-  if (numero === pensei) {
-    console.log(player.nome + " acertô mizeravi! ");
-    console.log("Numero de tentativas " + contTentativas);
+    if (numero < pensei) {
+      console.log(player.nome + " é maior que " + numero);
+    }
+    if (numero > pensei) {
+      console.log(player.nome + " é menor que " + numero);
+    }
+    if (numero === pensei) {
+      console.log(player.nome + " acertô mizeravi! ");
+      console.log("Numero de tentativas " + contTentativas);
 
-    player.pontuacao =
-      contTentativas < player.pontuacao || player.pontuacao === 0
-        ? contTentativas
-        : player.pontuacao;
+      player.pontuacao =
+        contTentativas < player.pontuacao || player.pontuacao === 0
+          ? contTentativas
+          : player.pontuacao;
 
-    jogadores.push(player);
-  }
-} while (numero !== pensei);
-
+      jogadores.push(player);
+    }
+  } while (numero !== pensei);
+  continuar();
+}
 console.log(jogadores);
+
+function continuar() {
+  var escolha = Number(prompt("Escolha \n 1-Continuar\n 2-Sair"));
+  switch (escolha) {
+    case 1:
+      var vpe = Number(
+        prompt(
+          "Escolha 1-Continuar com o mesmo Jogador \n 2- Criar novo jogador"
+        )
+      );
+      switch (vpe) {
+        case 1:
+          jogar();
+        case 2:
+          menuGeral();
+          break;
+      }
+    case 2:
+      break;
+  }
+}
+function menuGeral() {
+  var newGame = Number(prompt("1-Novo Jogo \n 2-Sair"));
+  switch (newGame) {
+    case 1:
+      player.nome = prompt("digite o nome do jogador");
+      jogar();
+      break;
+    case 2:
+      break;
+    default:
+      alert("Opção Invalida");
+      menuGeral();
+  }
+}
+
+//Menu Geral
+//JOGAR
+//CONTINUAR COM O MESMO JOGADOR
+//INICIAR COM NOVO JOGADOR
+
+//SAIR
