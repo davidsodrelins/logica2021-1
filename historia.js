718d8a9335c2c5365d9f59b5ff4449358d124035
David Sodré Lins <david.silva@acpgroup.com.br>
2021-04-22 20:52:44 -0300

    david...

--- historia.js
+++ historia.js
@@ -6,4 +6,5 @@ console.log("e Gohan tinha cabelos espetados.")
 console.log("Dina possuia cabelos dourados e olhos cinzas")
 console.log("Eles viviam pacificamente dentro da Muralha Maria")
 console.log("com medo de Freeza.")
+console.log("Dina ligou para Naruto dizendo que")
 
