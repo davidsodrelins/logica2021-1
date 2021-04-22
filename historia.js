9758b34637436dab06a2ff22f022eb68109fdba5
LucasSouza <82621071+LucasSouza2021@users.noreply.github.com>
2021-04-22 20:39:12 -0300

    Parte Jadson

--- historia.js
+++ historia.js
@@ -1,6 +1,6 @@
 console.log("Era uma vez...")
 console.log("uma menina chamada dine friz")
 console.log("Dina possuia cabelos dourados e olhos cinzas")
-
+console.log("Eles viviam pacificamente dentro da Muralha Maria")
 
 
