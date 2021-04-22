b8c7cb7f05aa19336408ca52bc4804dd5606befa
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-04-22 20:49:56 -0300

    Parte de Silvio

--- historia.js
+++ historia.js
@@ -1,5 +1,6 @@
 console.log("Era uma vez...")
 console.log("uma menina chamada dina fritz")
+console.log("que caminhava pela estrada de tijolos amarelos.")
 console.log("e um menino chamado Gohan")
 console.log("e Gohan tinha cabelos espetados.")
 console.log("Dina possuia cabelos dourados e olhos cinzas")
