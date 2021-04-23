3b3573e4692cc320db7d38ffb907a80f0f3ecea4
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-04-22 21:05:48 -0300

    De novo e again

--- historia.js
+++ historia.js
@@ -3,8 +3,12 @@ console.log("uma menina chamada Dina Fritz.")
 console.log("Que caminhava pela estrada de tijolos amarelos.")
 console.log("E um menino chamado Gohan.")
 console.log("Gohan tinha cabelos espetados, mas não o lavava.")
-console.log("Dina possuia cabelos dourados e olhos cinzas")
-console.log("Eles viviam pacificamente dentro da Muralha Maria")
+console.log("Dina possuia cabelos dourados e olhos cinzas azulados.")
+console.log("Eles viviam pacificamente dentro da Muralha Maria de tijolos de barro.")
 console.log("com medo de Freeza.")
 console.log("Dina ligou para Naruto dizendo que")
 console.log("Quando um jovem chamado Naruto veio da vila da folha e lhes ensinaram a arte sábia ninja")
+
+
+
+ 
